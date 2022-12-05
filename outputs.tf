ac3cf8369bfc5d8f27c5dd67b499f0ef2ca68a53
ArjunJMishra <arjun.j.mishra@pwc.com>
2022-12-06 00:37:08 +0530

    host

--- outputs.tf
+++ outputs.tf
@@ -19,9 +19,15 @@
 output "hana_instance_public_ips" {
   value = module.hana_host.instance_public_ips
 }
+
 output "hana_instance_private_ip" {
   value = module.hana_host.server_private_ip
 }
+
+output "hana_instance_private_ips" {
+  value = module.hana_host.server_private_ips
+}
+
 output "hana_instance_overlay_ip" {
   value = module.hana_host.overlay_ip
 }
