a1c702dcb655691d487ee909e3b1f205d2a8a79f
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2022-12-13 05:17:17 -0500

    Update ha_route_table.tf

--- modules/aws-sap-ascs-host/ha_route_table.tf
+++ modules/aws-sap-ascs-host/ha_route_table.tf
@@ -22,14 +22,14 @@ data "aws_route_table" "ha_route_table" {
 }
 
 resource "aws_route" "ha_route" {
-  count = var.enable_ha ? 1 : 0
+  #count = var.enable_ha ? 1 : 0
 
   route_table_id         = data.aws_route_table.ha_route_table.id
   destination_cidr_block = var.destination_cidr_block_for_overlay_ip_ASCS
   network_interface_id   = module.instance.network_interface_id[0]
 }
 resource "aws_route" "ha_route1" {
-  count = var.enable_ha ? 1 : 0
+  #count = var.enable_ha ? 1 : 0
 
   route_table_id         = data.aws_route_table.ha_route_table.id
   destination_cidr_block = var.destination_cidr_block_for_overlay_ip_ERS 
