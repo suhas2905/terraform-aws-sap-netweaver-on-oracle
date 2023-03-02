5c714f994ad73966734e88b44c93586ac7ee8365
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-03-02 08:06:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "oracle_host" {
 
 module "sap_ascs_host" {
   source  = "./modules/aws-sap-ascs-host"
-  enabled = false
+  enabled = var.enabled
 
   # Instance Count depending on the environment
   instance_count = 1
@@ -125,7 +125,7 @@ module "sap_ascs_host" {
 
 module "sap_ers_host" {
   source  = "./modules/aws-sap-ers-host"
-  enabled =  false
+  enabled =  var.enabled && var.enable_ha ? true : false
 
   # Instance Count depending on the environment
   instance_count = 1
@@ -170,7 +170,7 @@ module "sap_ers_host" {
 
 module "sap_app_host" {
   source  = "./modules/aws-sap-app-host"
-  enabled = false
+  enabled = var.enabled
 
   # Instance Count depending on the environment
   instance_count = var.as_instance_count
